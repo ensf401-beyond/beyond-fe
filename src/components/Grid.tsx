92ff678a96020010f36461e8111653f8d240cfdb
aleabalda <alessandro.baldassar@ucalgary.ca>
2024-02-10 01:07:34 -0700

    added dummy data and potential layout for obj's

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,3 +1,19 @@
-function Grid() {}
+import { DummyData } from "../data/DummyData";
+import GridCard from "./ui/GridCard";
+
+type GridItem = {
+    name: string,
+    image: string
+}
+
+function Grid() {
+    return(
+        <div id="grid-container">
+            {DummyData.map((val: GridItem, key: any) => (
+            <GridCard name={val.name} image={val.image} />
+          ))}
+        </div>
+    )
+}
 
 export default Grid;
--- src/components/ui/GridCard.tsx
+++ src/components/ui/GridCard.tsx
@@ -1,3 +1,16 @@
-function GridCard() {}
+type GridItem = {
+  name: string;
+  image: string;
+};
+
+function GridCard({ name, image }: GridItem) {
+  return (
+    <div className="grid-card">
+      <img className="sky-object-image" src={image} alt={name}></img>
+      <p className="sky-object-name">{name}</p>
+      <p className="sky-object-description">Click to view more!</p>
+    </div>
+  );
+}
 
 export default GridCard;
