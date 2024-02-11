fa7fe9435d860766f3608ecaa87e086e84ff8a7b
1saacH <isaacmhus@gmail.com>
2024-02-11 15:03:41 -0700

    too lazy to stash

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -9,9 +9,19 @@ function Profile() {
 
   return (
     <>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Tate_McRae_on_iHeartRadio_Canada_in_2023_%281%29.png/1200px-Tate_McRae_on_iHeartRadio_Canada_in_2023_%281%29.png" alt="PFP"/>
-      <h1>Username</h1>
-      <h2>email</h2>
+
+      <div className="profile-page">
+        <div className="profile-image-and-links">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Tate_McRae_on_iHeartRadio_Canada_in_2023_%281%29.png/1200px-Tate_McRae_on_iHeartRadio_Canada_in_2023_%281%29.png" alt="PFP" className="profile-pfp"/>
+
+        </div>
+        <div className="profile_feilds">
+          <h1>Username</h1>
+          <h2>email</h2>
+        </div>
+        
+      </div>
+      
     </>
   );
 }
