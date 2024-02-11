964975ac5e05d545e3087d1942c9d0175d999ade
1saacH <isaacmhus@gmail.com>
2024-02-11 15:54:45 -0700

    finished profile page till we get backend stuff

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,17 +1,66 @@
-
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Profile() {
 
 
 
+  const [name, setName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [location, setLocation] = useState<string>("");
+  const [pfp, setPfp] = useState<string>("");
+
+  const handleSubmit = (event : any) => {
+    event.preventDefault();
+    console.log(name , email, location);
+    // TODO: send the data to the backend
+    localStorage.setItem("Name", name);
+    localStorage.setItem("Email", email);
+    localStorage.setItem("Location", location);
+  }
+
+  const getInfo = () => {
+    setName(localStorage.getItem("Name") || "");
+    setEmail(localStorage.getItem("Email") || "");
+    setLocation(localStorage.getItem("Location") || "");
+    setPfp(localStorage.getItem("PFP") || "");
+  }
 
 
+  useEffect(() => {
+    getInfo();
+  }, []);
   return (
     <>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Tate_McRae_on_iHeartRadio_Canada_in_2023_%281%29.png/1200px-Tate_McRae_on_iHeartRadio_Canada_in_2023_%281%29.png" alt="PFP"/>
-      <h1>Username</h1>
-      <h2>email</h2>
+
+      <div className="profile-page">
+        <div className="profile-image-and-links">
+          <img src={pfp} alt="PFP" className="profile-pfp"/>
+        </div>
+        <div className="profile-feilds">
+          <form onSubmit={handleSubmit} className="profile-form">
+            <div className="profile-feild">
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" placeholder="Name" required onChange={(e) => setName(e.target.value)} value={name}/>
+            </div>
+            <div className="profile-feild">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" placeholder="Email" required onChange={(e) => setEmail(e.target.value)} value={email}/>
+            </div>
+            <div className="profile-feild">
+              <label htmlFor="location">Country</label>
+              <input type="text" id="Country" name="Country" placeholder="Country" required onChange={(e) => setLocation(e.target.value)} value={location}/>
+            </div>
+            <button type="submit" className="profile-submit small-button" >Confirm</button>
+          </form>
+          <button className='small-button'>
+            <Link to="/favorites" className="profile-link">Go to Favorites</Link>
+          </button>
+        </div>
+        
+      </div>
+      
     </>
   );
 }
