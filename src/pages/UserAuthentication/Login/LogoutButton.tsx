a040b9f89d46bfe303413b16a0ea000492380884
Rita <rita.boury@ucalgary.ca>
2024-02-19 20:07:52 -0700

    refactored google login/logout

--- src/pages/UserAuthentication/Login/LogoutButton.tsx
+++ src/pages/UserAuthentication/Login/LogoutButton.tsx
@@ -1,11 +1,9 @@
-import { GoogleLogout } from "react-google-login";
 import { useNavigate } from "react-router-dom";
-
-const clientID =
-  "138234664993-d0mvhfhmbq2vh2877oq6ub1v6ie1hbj9.apps.googleusercontent.com";
+import { googleLogout } from '@react-oauth/google';
 
 function LogoutButton({ handleLogout }: any) {
   const navigate = useNavigate();
+
   const onSuccess = () => {
     console.log("Successfully Logged Out!");
     handleLogout();
@@ -13,12 +11,14 @@ function LogoutButton({ handleLogout }: any) {
   };
 
   return (
-    <div id="signoutButton">
-      <GoogleLogout
-        clientId={clientID}
-        buttonText="Logout"
-        onLogoutSuccess={onSuccess}
-      />
+    <div className="flex-body">
+      <button id="customBtn" onClick={() => {
+        googleLogout();
+        onSuccess();
+      }}>
+        <span className="buttonText">Log Out </span>
+        <span className="googleIcon"></span>
+      </button>
     </div>
   );
 }
