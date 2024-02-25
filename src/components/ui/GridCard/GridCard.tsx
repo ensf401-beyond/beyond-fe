8b4274b0b18600d605b41f330b767d73c6e9157a
chantaeh <chantae.ho@ucalgary.ca>
2024-02-25 11:51:49 -0700

    Refactor with grid-card-text

--- src/components/ui/GridCard/GridCard.tsx
+++ src/components/ui/GridCard/GridCard.tsx
@@ -31,8 +31,10 @@ function GridCard({ name, image }: GridItem) {
   return (
     <div className="grid-card">
       <img className="sky-object-image" src={image} alt={name}></img>
-      <p className="sky-object-name">{name}</p>
-      <p className="sky-object-description">Click to view more!</p>
+      <div className="grid-card-text">
+        <p className="sky-object-name">{name}</p>
+        <p className="sky-object-description">Click to view more!</p>
+      </div>
     </div>
   );
 }
