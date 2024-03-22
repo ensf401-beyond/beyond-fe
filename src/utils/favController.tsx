c3993f284e0dffe5d641ad20df1c8451189591c6
chantaeh <chantae.ho@ucalgary.ca>
2024-03-21 20:48:35 -0600

    Add addFavourite function

--- src/utils/favController.tsx
+++ src/utils/favController.tsx
@@ -20,4 +20,32 @@ function getFavourites(email : string) : Promise<any> {
         });
 }
 
-export { getFavourites };
+function addFavourite(email : string, ngc : number) : Promise<any> {
+    console.log('adding favourite');
+
+    var requestBody =  JSON.stringify({ email: email, ngc: ngc });
+
+
+    return fetch(`https://imolyxerm4cc73s75gdxrgvsru0dbvzn.lambda-url.ca-central-1.on.aws/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: requestBody
+    })
+        .then(async response => {
+            console.log(await response.json());
+            return response.status;
+        })
+        .catch(error => {
+            console.error(error);
+            // handle any errors
+            throw error;
+        });
+}
+
+// 
+// https://log7pdtoidp72cwxjkvdxqrou40sqita.lambda-url.ca-central-1.on.aws/
+// (takes in  email and ngc as query parameters, uses http DELETE method, returns status 200 and "Favourite deleted successfully" if successful.)
+
+export { getFavourites, addFavourite };
